f36ebb95783c0678bce82985c2c3a4c0480ef3b9
Naseem <naseemmuhammad700@gmail.com>
2023-08-08 12:00:10 -0700

    working on dev

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -34,47 +34,47 @@ window.onscroll=()=>{
     shoppingcart.classList.remove('active');
     loginform.classList.remove('active');
 }
-var swiper = new Swiper(".product-slider", {
-    loop:true,
-    spaceBetween: 20,
-    autoplay:{
-        delay:7500,
-        disableOnInteraction:false,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
+// var swiper = new Swiper(".product-slider", {
+//     loop:true,
+//     spaceBetween: 20,
+//     autoplay:{
+//         delay:7500,
+//         disableOnInteraction:false,
+//     },
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
         
-      },
-      768: {
-        slidesPerView: 2,
+//       },
+//       768: {
+//         slidesPerView: 2,
         
-      },
-      1024: {
-        slidesPerView: 3,
+//       },
+//       1024: {
+//         slidesPerView: 3,
         
-      },
-    },
-  });
-  var swiper = new Swiper(".review-slider", {
-    loop:true,
-    spaceBetween: 20,
-    autoplay:{
-        delay:7500,
-        disableOnInteraction:false,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
+//       },
+//     },
+//   });
+//   var swiper = new Swiper(".review-slider", {
+//     loop:true,
+//     spaceBetween: 20,
+//     autoplay:{
+//         delay:7500,
+//         disableOnInteraction:false,
+//     },
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
         
-      },
-      768: {
-        slidesPerView: 2,
+//       },
+//       768: {
+//         slidesPerView: 2,
         
-      },
-      1024: {
-        slidesPerView: 3,
+//       },
+//       1024: {
+//         slidesPerView: 3,
         
-      },
-    },
-  });
+//       },
+//     },
+//   });
